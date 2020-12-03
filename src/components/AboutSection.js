c4d6dcae4b2952ca5d5722b3abb48ff87312caff
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-03 15:09:15 +0100

    basic styling of services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,11 +2,11 @@
 import home1 from "../img/home1.png";
 
 // Styling
-import styled from "styled-components";
+import { Container, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <About>
+    <Container>
       <Description>
         <div className="title">
           <Hide>
@@ -30,40 +30,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="Man with a camera" />
       </Image>
-    </About>
+    </Container>
   );
 };
-
-// Styled Components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
