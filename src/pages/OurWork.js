bfae18abf6656ad38a0229d741c057e171831c95
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-12-07 11:13:29 +0100

    setup all imports for our work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,3 +1,14 @@
+// Imports from Router
+import { Link } from "react-router-dom";
+
+// Import of images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
+// Import Styles
+import styled from "styled-components";
+
 const OurWork = () => {
   return (
     <div>
